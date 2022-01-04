f8d8225bc48e296f7dd9fea0bbdb0b1c5574cff5
mysoyul <vega2k_@naver.com>
2022-01-04 16:16:04 +0900

    App.js update

--- src/App.js
+++ src/App.js
@@ -3,12 +3,26 @@ import Form from './components/Form';
 import TodoItemList from './components/TodoItemList';
 import TodoListTemplate from './components/TodoListTemplate';
 class App extends Component {
+  id = 3; // 이미 0,1,2 가 존재하므로 3으로 설정
+
+  //상태변수를 가진 state 객체선언
+  state = {
+    todo: '',
+    todos: [
+      { id: 0, text: '리액트 소개', checked: false },
+      { id: 1, text: '리액트 구조', checked: true },
+      { id: 2, text: '리액트 사용', checked: false }
+    ]
+  }
+  //Event Handler 메서드선언
+  
+
   render() {
     return (
       <div>
-          <TodoListTemplate form={<Form />}>
-            <TodoItemList />
-          </TodoListTemplate>
+        <TodoListTemplate form={<Form />}>
+          <TodoItemList />
+        </TodoListTemplate>
       </div>
     );
   }

3c3130c356003cf1e30dd5beea66c27a1fdaf057
mysoyul <vega2k_@naver.com>
2022-01-04 15:40:09 +0900

    TodoItemList.js add

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import Form from './components/Form';
 import TodoListTemplate from './components/TodoListTemplate';
 class App extends Component {
   render() {
     return (
       <div>
-          <TodoListTemplate form={<div>Form</div>}>
+          <TodoListTemplate form={<Form />}>
             <div>TodoList</div>
           </TodoListTemplate>
       </div>
---
+++ src/components/TodoItemList.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class TodoItemList extends Component {
+    render() {
+        const {todos, myToggle, myRemove} = this.props;
+
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default TodoItemList;

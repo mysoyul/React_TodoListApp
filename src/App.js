03ea5e4abc2b853851b32cd86830770a204bdb16
mysoyul <vega2k_@naver.com>
2022-01-04 15:51:34 +0900

    TodoItem call

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import Form from './components/Form';
+import TodoItemList from './components/TodoItemList';
 import TodoListTemplate from './components/TodoListTemplate';
 class App extends Component {
   render() {
     return (
       <div>
           <TodoListTemplate form={<Form />}>
-            <div>TodoList</div>
+            <TodoItemList />
           </TodoListTemplate>
       </div>
     );
--- src/components/TodoItemList.js
+++ src/components/TodoItemList.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import TodoItem from './TodoItem';
+
 
 class TodoItemList extends Component {
     render() {
@@ -6,7 +8,9 @@ class TodoItemList extends Component {
 
         return (
             <div>
-                
+                <TodoItem text="todo1" checked={false} />
+                <TodoItem text="todo2" checked={true} />
+                <TodoItem text="todo3" checked={false} />
             </div>
         );
     }

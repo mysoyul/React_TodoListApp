0836160a82f6e7a5791745570ae962a17b01e767
mysoyul <vega2k_@naver.com>
2022-01-04 16:23:19 +0900

    App.js update

--- src/App.js
+++ src/App.js
@@ -15,7 +15,31 @@ class App extends Component {
     ]
   }
   //Event Handler 메서드선언
-  
+  handleChange = (e) => {
+    this.setState({
+      todo: e.target.value // todo 의 다음 바뀔 값
+    });
+  }
+  handleCreate = () => {
+    const { todo, todos } = this.state;
+    const todoObj = {
+      id: this.id++,
+      text: todo,
+      checked: false
+    };
+
+    this.setState({
+      todo: '', // todo 초기화
+      // ...(spread operator)을 사용하여 todo객체를 배열에 추가
+      todos: [...todos, todoObj]
+    });
+  }
+  handleKeyPress = (e) => {
+    // 눌려진 키가 Enter 이면 handleCreate 호출
+    if (e.key === 'Enter') {
+      this.handleCreate();
+    }
+  }
 
   render() {
     return (

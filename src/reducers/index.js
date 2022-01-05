0a0e79b3763ad628f7e18e0f172dbff37731b76f
mysoyul <vega2k_@naver.com>
2022-01-05 16:39:53 +0900

    reducer add

---
+++ src/reducers/index.js
@@ -0,0 +1,22 @@
+import * as types from '../actions';
+
+//state 객체 초기화
+const initialState = {
+    todos: [
+        {
+            id: 0,
+            text: '',
+            checked: false,
+        }
+    ]
+};
+
+//Reducer 함수 정의
+export const toDoReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case types.FETCH_TODOS:
+            return Object.assign({}, state, { todos: action.payload });
+        default:
+            return state;
+    }
+}
